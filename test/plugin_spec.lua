6b46307a0fa4426a6942a6af76634bfa9738e8d9
Craig Miller <2132647+craig-miller@users.noreply.github.com>
2025-05-02 17:16:24 -0700

    Disable gh automated tests because we don't have any.

--- test/plugin_spec.lua
+++ test/plugin_spec.lua
@@ -1,8 +1,8 @@
-local example = require('nvim-plugin-template').example
+-- local example = require('nvim-plugin-template').example
 
-describe('neovim plugin', function()
-  it('work as expect', function()
-    local result = example()
-    assert.is_true(result)
-  end)
-end)
+-- describe('neovim plugin', function()
+--   it('work as expect', function()
+--     local result = example()
+--     assert.is_true(result)
+--   end)
+-- end)
